95bd999b52ba8c69661d15d65867331cb55f60c6
Kenneth Love <kenneth@gigantuan.net>
2017-01-11 17:39:02 -0800

    Tests pass! Needs type hints

--- address_book.py
+++ address_book.py
@@ -11,49 +11,43 @@ class Person:
         self.email = email
 
     def __str__(self):
-        return self.name
-
-    def __repr__(self):
-        return str(self.__dict__)
-
-    def toJSON(self):
-        return json.dumps(self.__dict__)
+        return f'{self.name}: {self.email}'
 
 
-class AddressBook:
-    def __init__(self, filename):
-        self.file = os.path.join(CURRENT_DIR, filename)
-        self.records = self._load_all()
+class AddressBook(set):
+    def __init__(self, iterable=None, filename=None):
+        self.file = os.path.join(CURRENT_DIR, filename or 'address_book.yml')
+        self._load_all()
+        super().__init__(iterable or [])
 
     def _load_all(self):
         try:
-            with open(self.file, 'r') as file:
+            with open(self.file, 'r+') as file:
                 records = yaml.load(file.read())
         except FileNotFoundError:
-            return []
+            pass
         else:
-            return [Person(person['name'], **person) for person in records]
+            for record in records:
+                self.add(record)
 
     def _save(self):
-        with open(self.file, 'a') as file:
-            file.write(yaml.dump(self.records))
-
-    def __iter__(self):
-        yield from self.records
-
-    def __contains__(self, person):
-        return len(list(self.search(person)))
+        with open(self.file, 'w+') as file:
+            file.write(yaml.dump(self))
 
     def clear(self):
-        os.unlink(self.file)
+        try:
+            os.unlink(self.file)
+        except FileNotFoundError:
+            pass
 
     def search(self, name):
         return filter(lambda p: p.name == name, self)
 
-    def add(self, name, email):
-        if name in self:
-            raise KeyError("{} already exists".format(name))
-        else:
-            self.records.append(Person(name, email))
+    def add_record(self, person):
+        self.add(person)
+        self._save()
+
+    def remove_record(self, person):
+        self.remove(person)
         self._save()
 
--- test_address_book.py
+++ test_address_book.py
@@ -5,12 +5,29 @@ import address_book
 
 class AddressBookTests(unittest.TestCase):
     def setUp(self):
-        self.address_book = address_book.AddressBook("test.yml")
+        self.address_book = address_book.AddressBook(filename="test.yml")
+        self.p1 = address_book.Person("One", "one@example.com")
+        self.p2 = address_book.Person("Two", "two@example.com")
 
     def tearDown(self):
         # self.address_book.clear()
         pass
 
     def test_add_person(self):
-        self.address_book.add("Kenneth Love", "kenneth@teamtreehouse.com")
-        self.assertIn("Kenneth Love", self.address_book)
+        self.address_book.add_record(self.p1)
+        self.assertIn(self.p1, self.address_book)
+
+    def test_add_two_people(self):
+        self.address_book.add_record(self.p1)
+        self.address_book.add_record(self.p2)
+        self.assertEqual(len(self.address_book), 2)
+
+    def test_add_repeat(self):
+        self.address_book.add_record(self.p1)
+        self.address_book.add_record(self.p1)
+        self.assertEqual(len(self.address_book), 1)
+
+    def test_remove(self):
+        self.address_book.add_record(self.p1)
+        self.address_book.remove_record(self.p1)
+        self.assertNotIn(self.p1, self.address_book)
